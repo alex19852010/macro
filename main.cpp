7b954bfa9cf7f76ba863643c46ae42061e97a9c7
Alex Bogushenko <bogushenkoas@ukr.net>
2023-05-26 11:52:31 +0300

    1macro

--- main.cpp
+++ main.cpp
@@ -3,10 +3,34 @@
 
 using namespace std;
 
+#define  WINTER  1
+#define  SPRING  2
+#define  SUMMER  3
+#define  AUTUMN  4
+
 
 main()
 {
-cout << "hello alex";
 
-return 0;
+#define SEASON SPRING
+
+
+ #if SEASON == WINTER
+ cout << "winter" << endl;
+
+ #elif SEASON == SPRING
+ cout << "spring" << endl;
+
+ #elif SEASON == SUMMER
+ cout << "summer" << endl;
+
+ #elif SEASON == AUTUMN
+ cout << "autumn" << endl;
+
+ #else
+ cout << "error season" << endl;
+
+ #endif
+
+ return 0;
 }   
